4485883a0a06a60b8f2b744865664ec50a0b5af7
llamakking <57610847+llamakking@users.noreply.github.com>
2022-01-10 15:42:02 -0500

    Removed office points

--- Back_To_Hue/lib/main.dart
+++ Back_To_Hue/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'src/locations.dart' as locations;
+// import 'src/locations.dart' as locations;
 
 void main() {
   runApp(const MyApp());
@@ -14,24 +14,24 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  final Map<String, Marker> _markers = {};
-  Future<void> _onMapCreated(GoogleMapController controller) async {
-    final googleOffices = await locations.getGoogleOffices();
-    setState(() {
-      _markers.clear();
-      for (final office in googleOffices.offices) {
-        final marker = Marker(
-          markerId: MarkerId(office.name),
-          position: LatLng(office.lat, office.lng),
-          infoWindow: InfoWindow(
-            title: office.name,
-            snippet: office.address,
-          ),
-        );
-        _markers[office.name] = marker;
-      }
-    });
-  }
+  // final Map<String, Marker> _markers = {};
+  // Future<void> _onMapCreated(GoogleMapController controller) async {
+  //   final googleOffices = await locations.getGoogleOffices();
+  //   setState(() {
+  //     _markers.clear();
+  //     for (final office in googleOffices.offices) {
+  //       final marker = Marker(
+  //         markerId: MarkerId(office.name),
+  //         position: LatLng(office.lat, office.lng),
+  //         infoWindow: InfoWindow(
+  //           title: office.name,
+  //           snippet: office.address,
+  //         ),
+  //       );
+  //       _markers[office.name] = marker;
+  //     }
+  //   });
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -42,12 +42,12 @@ class _MyAppState extends State<MyApp> {
           backgroundColor: Colors.green[700],
         ),
         body: GoogleMap(
-          onMapCreated: _onMapCreated,
+          // onMapCreated: _onMapCreated,
           initialCameraPosition: const CameraPosition(
             target: LatLng(0, 0),
             zoom: 2,
           ),
-          markers: _markers.values.toSet(),
+          // markers: _markers.values.toSet(),
         ),
       ),
     );
